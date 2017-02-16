a70808288110e75cb83f834a9651a193cd5e381e
adid1d4 <adsharma.d1d4@gmail.com>
2017-02-17 02:11:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.get('/gi/article3',function(req,res) {
     res.sendFile(path.join(__dirname,'gi','article3.html'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
