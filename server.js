56b79b77af6e5a336cba21fcbfc78e233bbb67d6
adid1d4 <adsharma.d1d4@gmail.com>
2017-02-17 12:18:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/article-one',function(req,res) {
-    res.sendFile(path.join(__dirname,'article1.html'));
-});
-app.get('/ui/article2',function(req,res) {
-    res.sendFile(path.join(__dirname, "ui", 'article2.html'));
-});
+ var morgan = require('morgan');
+ var path = require('path');
+ 
+ var app = express();
+ app.use(morgan('combined'));
+ 
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+ app.get('/article-one',function(req,res) {
+     res.sendFile(path.join(__dirname,'article1.html'));
+ });
+  app.get('/ui/article2',function(req,res) {
+      res.sendFile(path.join(__dirname, "ui", 'article2.html'));
+  });
 app.get('/gi/article3',function(req,res) {
-    res.sendFile(path.join(__dirname,'gi','article3.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+      res.sendFile(path.join(__dirname,'gi','article3.html'));
+  });
+  app.get('/ui/style.css', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
+ 
+ app.get('/ui/madi.png', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ });
+ 
+ 
+ var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+ app.listen(8080, function () {
+   console.log(`IMAD course app listening on port ${port}!`);
+ });
